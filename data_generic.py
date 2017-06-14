4a1e1813030f1171b133cdd812667387112d1ef6
Alksandr Drozd <alexander.drozd@gmail.com>
2017-06-14 17:34:02 +0900

    dtype customizable for vector to onehot

--- data_generic.py
+++ data_generic.py
@@ -1,9 +1,7 @@
-import json
-import os
 import numpy as np
 
 
-def class_to_onehot(vector, cnt_classes):
-    res = np.zeros((vector.shape[0], cnt_classes), dtype=np.bool)
+def class_to_onehot(vector, cnt_classes, dtype=np.bool):
+    res = np.zeros((vector.shape[0], cnt_classes), dtype=dtype)
     res[np.arange(len(vector)), vector] = 1
     return res
