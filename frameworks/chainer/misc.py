a58a6251fbaa408c22990cb73da43f85facc926f
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-06-25 00:17:13 +0900

    housekkeping

--- frameworks/chainer/misc.py
+++ frameworks/chainer/misc.py
@@ -1,6 +1,7 @@
 from chainer.serializers import NpzDeserializer
 import numpy as np
 
+
 def load_model_from_trainer_npz(path, model):
     with np.load(path) as f:
         d = NpzDeserializer(f, path="updater/model:main/")
