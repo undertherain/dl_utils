6699b6f05a0cc2c8fb47cfa9bef3514460c72693
Alksandr Drozd <alexander.drozd@gmail.com>
2017-06-30 17:05:31 +0900

    add cmp loss plot

--- dl_utils/debug/visualization/plot.py
+++ dl_utils/debug/visualization/plot.py
@@ -32,4 +32,29 @@ def cmp_experiments(l):
 #            frame.set_facecolor('white')
         plt.tick_params(axis='both', which='major', labelsize=18)
 
-# cmp_experiments(experiments[-2:])
+
+def cmp_loss(l):
+    plt.subplot(1, 1, 1)
+    ymin = []
+    ymax = []
+    for exp in l:
+        hist = exp.history
+        loss = np.array(hist["loss"])
+        val_loss = np.array(hist["val_loss"])
+        ymin.append(loss.min())
+        ymin.append(val_loss.min())
+        ymax.append(loss.max())
+        ymax.append(val_loss.max())
+        # print(ymax)
+        plt.scatter(loss, val_loss, label=exp.descr, linewidth=2, alpha=0.7)
+        plt.xlabel("loss", size=16)
+        plt.ylabel("validation loss", size=16)
+    ymax = min(max(ymax), 1)
+    plt.ylim(min(ymin), ymax)
+    plt.xlim(min(ymin), ymax)
+#   legend = plt.legend(loc=1, prop={'size': 18})
+    plt.tick_params(axis='both', which='major', labelsize=18)
+#   legend=plt.legend(loc=1, prop={'size':18},bbox_to_anchor=(2.1, 1.6, 0, 0))
+#   frame = legend.get_frame()
+#   frame.set_alpha(0)
+#   frame.set_facecolor('white')
