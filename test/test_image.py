2fb312849a46472fa7c6d94f14549cf460179af3
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-07-17 09:49:26 -0700

    unittests

--- test/test_image.py
+++ test/test_image.py
@@ -1,10 +1,13 @@
 import unittest
 from dl_utils.imaging import ImageLoader
 
-il =  ImageLoader()
+
+il = ImageLoader()
+path_img = "dl_utils/imaging/test_data/ireland.jpg"
 
 
 class Tests(unittest.TestCase):
 
-    def test_create(self):
-        pass
+    def test_load(self):
+        img = il.load_image(path_img)
+        print(img.shape)
